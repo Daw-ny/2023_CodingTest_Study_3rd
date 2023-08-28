a8d3ee67f1e5c429182f5c8bbbd11e6ca5f109be
Da-Woon Kim <76687996+Daw-ny@users.noreply.github.com>
2023-08-28 18:19:54 +0900

    Create 10_어_느새_부터_힙.py

---
+++ DW/인프런/chap05/10_어_느새_부터_힙.py
@@ -0,0 +1,19 @@
+# module
+import sys
+import heapq
+
+# input
+#sys.stdin = open("input.txt", "rt")
+
+# 최소 힙
+Hop = []
+while True:
+    n = int(input())
+    if n == -1:
+        break
+
+    elif n == 0 and Hop:
+        print(heapq.heappop(Hop))
+
+    elif n not in [-1, 0]:
+        heapq.heappush(Hop, n)
