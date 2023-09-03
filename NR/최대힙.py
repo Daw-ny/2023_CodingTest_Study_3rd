93ce95c003fd4dd7c9db438aff14f1b6a8619d0e
Narin <62434201+nrgolden16@users.noreply.github.com>
2023-09-03 21:38:30 +0900

    Create 최대힙.py

---
+++ NR/최대힙.py
@@ -0,0 +1,15 @@
+from heapq import heappop, heapify, heappush
+
+heap=[]
+while True:
+    n = int(input())
+
+    if n==-1:
+        break
+    if n==0:
+        if len(heap)==0:
+            print(-1)
+        else:
+            print(heappop(heap)[1])
+    else:
+        heappush(heap,(-n, n))
