20209b733b8f175f8b32e9cbaf7908beb488b88f
Da-Woon Kim <76687996+Daw-ny@users.noreply.github.com>
2023-09-04 17:55:23 +0900

    Update 01_bisect.py

--- DW/인프런/chap06/01_bisect.py
+++ DW/인프런/chap06/01_bisect.py
@@ -9,7 +9,10 @@ n = int(input())
 
 
 def ten_to_two(num, binum):
+    # 몫 나머지 출력
     p, q = divmod(num, 2)
+
+    # 몫이 0이면 이진수 출력 아니면 몫을 입력해서 한번 더 진행
     return str(q) + binum if p == 0 else ten_to_two(p, str(q) + binum)
 
 
