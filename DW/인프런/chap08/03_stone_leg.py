3cf78edd76a39fb8495f1af33240b8dfa1c94d7e
Da-Woon Kim <76687996+Daw-ny@users.noreply.github.com>
2023-10-22 11:22:55 +0900

    Create 03_stone_leg.py

---
+++ DW/인프런/chap08/03_stone_leg.py
@@ -0,0 +1,15 @@
+# module
+import sys
+
+# input
+#sys.stdin = open("input.txt", "rt")
+
+# 돌다리 건너기
+n = int(input())
+dp = [0] * (n+2)
+dp[1] = 1
+dp[2] = 2
+for i in range(3, n+2):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n+1])
